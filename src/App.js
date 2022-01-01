7cf22c8bc6385cbed0dad7b681660bfcf77315ec
mecancode <mecancode@gmail.com>
2022-01-01 07:49:33 -0500

    jsx

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+
+const title = "React" //define outside the function is best.  Gets rendered one time
+
+//this is a basic javaScript object whose parts can be used in the DOM
+const welcome = {
+  greeting: "Hey",
+  title: "React",
+};
+
+//Note that I can create a function outside the main App, then call this function inside the App. My function takes a parameter, which I must provide when I call it.
+
+function getTitle(title) {
+  return title;
+}
 
 function App() {
+//if variable defined here, then it should be dependent on parameters in the function.  gets rendered every time the function runs.
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    //This mixture of html and javaScript is called JSX.  This is what gets displayed in the browser.
+  
+    //When creating inputs in React use htmlFor instead of for.
+    <div>
+      <h1>
+        Hello there {title}
+        {welcome.greeting} {welcome.title}
+        Good morning {getTitle("Jocelyn")}
+      </h1>
+
+      
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" placeholder="Search for anything"/>
+
+
     </div>
+      
+  
   );
 }
 
